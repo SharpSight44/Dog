502db7b4faf2949a77bf0d24e8f6e5bbbcbf7e74
SharpSight44 <Leoest011@gmail.com>
2022-07-22 17:04:33 +0000

    socials half resp

--- src/social.js
+++ src/social.js
@@ -14,19 +14,19 @@ return (
 
 <h1  > Instagram</h1>
     
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div className="socials" >
   <InstagramEmbed  style={{margin:"20px"}} url="https://www.instagram.com/p/CfDO-veMj_D/?utm_source=ig_web_copy_link" width={328} />
   <InstagramEmbed style={{margin:"20px"}} url="https://www.instagram.com/p/Cd85rhJpYvl/?utm_source=ig_web_copy_link" width={328} />
   <InstagramEmbed style={{margin:"20px"}} url="https://www.instagram.com/p/CesELtss4VB/?utm_source=ig_web_copy_link" width={328} />
 </div>
 <h1> TikTok</h1>
-<div style={{ display: 'flex', justifyContent: 'center' }}>
+<div className="socials">
   <TikTokEmbed style={{margin:"20px"}} url="https://www.tiktok.com/@mountdoodlesfam/video/7093265244381809962?is_from_webapp=1&sender_device=pc&web_id=7076915022273971755" width={325} />
   <TikTokEmbed style={{margin:"20px"}} url="https://www.tiktok.com/@mountdoodlesfam/video/7102985531096649003?is_copy_url=1&is_from_webapp=v1" width={325}/>
 </div>
 <h1> Facebook </h1> 
-<div style={{ display: 'flex', justifyContent: 'center' }}>
-  <FacebookEmbed url="https://www.facebook.com/mountdoodlesfam/posts/pfbid02k7moLYSA28pryij8uHZMprJFZAX6cXWa5mnqzZPdY3zTwWqJAwQNybo8Sc6N6uDhl" width={550} />
+<div className="socials" >
+  <FacebookEmbed id="face" url="https://www.facebook.com/mountdoodlesfam/posts/pfbid02k7moLYSA28pryij8uHZMprJFZAX6cXWa5mnqzZPdY3zTwWqJAwQNybo8Sc6N6uDhl" width={400} />
 </div>
     </ div >
 
