d0407801a0d4caf0fafe6482fbbe505e227b50fd
SharpSight44 <Leoest011@gmail.com>
2022-07-30 01:30:29 +0000

    contact

--- src/contactUs.js
+++ src/contactUs.js
@@ -1,18 +1,19 @@
 import React from "react";
+import Contact from './pictures/contact.png';
 
 
 
 export const ContactUs = ()=>{
 
 return(
-<div style={{background:"rgba(195, 232, 232, .5)",height:"85vh", paddingBottom:"2%"}}>
-
-<div>
-<h1>Location</h1>
+<div style={{background:"rgba(125, 206, 19, .4)",height:"85vh", paddingBottom:"2%"}}>
+<div style={{marginLeft:"10%", paddingTop:"5%"}}><img src={Contact} /></div>
+<div className="contact">
+<h6>Location</h6>
 <p>Central Florida</p>
-<h1>Phone</h1>
+<h6>Phone</h6>
 <p>(786)774-3836</p>
-<h1>Email</h1>
+<h6>Email</h6>
 <p>MountDoodlesFam@gmail.com</p>
 </div>
 
