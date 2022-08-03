c7a9c645ec4807feb7dd6d9b3b225ea4a68b7286
SharpSight44 <Leoest011@gmail.com>
2022-08-03 23:16:58 +0000

    update55

--- src/litter.js
+++ src/litter.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import './aboutBreeder.css';
-import pupp1 from './pictures/pupp1.jpg';
-import pupp2 from './pictures/pupp2.jpg';
-import pupp3 from './pictures/pupp3.jpg';
-import pupp4 from './pictures/pupp4.jpg';
+import buddy4 from './pictures/buddy4.jpg';
 import logoPups from './pictures/logoPups.png';
 import Good from "./pictures/HomeRun.png";
-const pupps = [pupp1,pupp2,pupp3,pupp4];
+import millie2 from './pictures/millie2.jpg';
+
+
+
+
 export default function Litter() {
 
 
@@ -17,8 +18,9 @@ export default function Litter() {
         <>
         <div id="topLit">
 <div className="logoPup"><img src={logoPups}/></div>
-<div id="pupHouse">{pupps.map((item, index)=> <div className="pupps" key={index}><img className="puppsImg"  src={item}/> <div style={{marginLeft:"35%", marginTop:"10%"}}> <div className="select">Select</div>      <a href="https://www.gooddog.com/breeders/mount-doodles-fam-florida#breed_id=124" target="_blank" rel="noreferrer"> <img style={{width:"150px"}} atl="dog"src={Good} /> </a>
-</div></div>)}</div>
+<div id="pupHouse"> </div>
+<div style={{width:"500px", height:"350px", background:"white", borderRadius:"10px", border:" 2px solid #f5f5f5"}}> <img style={{marginTop:"4%",marginLeft:"12%",borderRadius:"50%", width:"200px", height:"200px", objectFit:"cover"}} src={buddy4}/><img style={{marginTop:"4%",borderRadius:"50%", width:"200px", height:"200px", objectFit:"cover", marginLeft:"-40px"}} src={millie2}/>
+<div style={{fontSize:"42px", fontWeight:"500", marginLeft:"20%"}}>Buddy x Millie </div><div style={{fontSize:"32px", fontWeight:"400", marginLeft:"23%"}}>Born March 28th</div></div>
 
 
 
